1b1293c8167bf894946e34ff6bb4030b5b1d4d25
seed <sd.ghadir@gamil.com>
2023-02-19 18:48:25 +0330

    update builder

--- derror/builder.go
+++ derror/builder.go
@@ -1,39 +1,43 @@
 package derror
 
 type (
-	errorBuilder struct {
+	builder struct {
 		code    int
 		desc    string
 		message string
 		trace   string
 	}
 
-	ErrorBuilder interface {
-		Code(code int) ErrorBuilder
-		Message(message string) ErrorBuilder
-		Desc(desc string) ErrorBuilder
+	Builder interface {
+		Code(code int) Builder
+		Message(message string) Builder
+		Desc(desc string) Builder
 		Build() ServerError
 	}
 )
 
-var _ ErrorBuilder = (*errorBuilder)(nil)
+var _ Builder = (*builder)(nil)
 
-func (b *errorBuilder) Code(code int) ErrorBuilder {
+func NewBuilder() *builder {
+	return &builder{}
+}
+
+func (b *builder) Code(code int) Builder {
 	b.code = code
 	return b
 }
 
-func (b *errorBuilder) Message(message string) ErrorBuilder {
+func (b *builder) Message(message string) Builder {
 	b.message = message
 	return b
 }
 
-func (b *errorBuilder) Desc(desc string) ErrorBuilder {
+func (b *builder) Desc(desc string) Builder {
 	b.desc = desc
 	return b
 }
 
-func (b *errorBuilder) Build() ServerError {
+func (b *builder) Build() ServerError {
 	return &serverError{
 		message: b.message,
 		code:    b.code,
