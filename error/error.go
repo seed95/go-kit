27d790255d06adad274f57e2b69cd5c78f47d5a3
seed <sd.ghadir@gamil.com>
2023-02-19 10:38:05 +0330

    add error

---
+++ error/error.go
@@ -0,0 +1,53 @@
+package error
+
+import (
+	"fmt"
+)
+
+type ServerError interface {
+	error
+	Status() int
+	Message() string
+	Description() string
+	//GrpcStatusCode() codes.Code
+}
+
+type serverError struct {
+	code    int
+	message string // locale key
+	desc    string
+}
+
+var _ ServerError = (*serverError)(nil)
+
+func (se *serverError) SetDesc(desc string) ServerError {
+	se.desc = desc
+	return se
+}
+
+func (se *serverError) Error() string {
+	if len(se.desc) != 0 {
+		return fmt.Sprintf("(status_code: %d) %s, desc: %s", se.code, se.message, se.desc)
+	}
+	return fmt.Sprintf("(status_code: %d) %s", se.code, se.message)
+}
+
+func (se *serverError) Status() int {
+	return se.code
+}
+
+func (se *serverError) Message() string {
+	return se.message
+}
+
+func (se *serverError) Description() string {
+	return se.desc
+}
+
+//func (se *serverError) GrpcStatusCode() codes.Code {
+//	status, ok := coma.GrpcCodes[se.code]
+//	if !ok {
+//		return codes.Unknown
+//	}
+//	return status
+//}
